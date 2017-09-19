95c9c49dca54a67d97e46a86fde7e482187688a2
rikhallar <forlidzen@gmail.com>
2017-09-20 00:41:52 +0300

    Renamed coin sides

--- coin_flip.py
+++ coin_flip.py
@@ -1,14 +1,14 @@
 import random
 
 count = 0
-orel = 0
-reshka = 0
+heads = 0
+tails = 0
 while count < 100:
     coin_flip = random.randint(1, 2)
     if coin_flip == 1:
-        orel += 1
+        heads += 1
     if coin_flip == 2:
-        reshka += 1
+        tails += 1
     count += 1
-print (orel)
-print (reshka)
+print (heads)
+print (tails)
